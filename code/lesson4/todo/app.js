a4b4303039ba3f6b8b59e40a1e2ac6187bb2ade5
Tobias Schulz-Hess <tobias.schulz-hess@xing.com>
2022-05-06 20:15:29 +0200

    Added delete function

--- code/lesson4/todo/app.js
+++ code/lesson4/todo/app.js
@@ -22,6 +22,18 @@ app.post("/todo", (req, res) => {
   todoliste.push(newToDoListObject);
   res.send({ todos: todoliste });
 });
+app.delete("/todo/:id", (req, res) => {
+  console.log(req.params);
+  // ID is a string
+  const id = parseInt(req.params.id);
+  // parseInt makes a string a number, now id is a number
+
+  // find element with id == 1 and delete it
+  todoliste = todoliste.filter((eintrag_in_der_todoliste) => eintrag_in_der_todoliste.id != id);
+
+  console.log(`Element ${id} deleted`);
+  res.send({ todos: todoliste });
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
