d7497044dcc1ef26a1f7f0a27fa7449f7342221e
Tobias Schulz-Hess <tobias.schulz-hess@xing.com>
2022-05-12 21:04:10 +0200

    Finished CRUD functionality for MySQL

--- code/lesson7/app.js
+++ code/lesson7/app.js
@@ -15,21 +15,64 @@ const express = require("express");
 const PORT = 3000;
 const app = express();
 
+const BAD_WORDS = ["doof"];
+
 app.use(express.json());
 
-app.get("/todo", (req, res) => {
-  pool.query("SELECT * FROM todos", (error, results, fields) => {
-    console.log(results);
-    res.send({ ok: true, result: results });
-  });
+app.get("/todo/:offset/:limit", (req, res) => {
+  console.log(req.params);
+  console.log(req.body);
+  pool.query(
+    "SELECT * FROM todos LIMIT ? OFFSET ?",
+    [parseInt(req.params.limit), parseInt(req.params.offset)],
+    (error, results, fields) => {
+      res.send({ ok: true, result: results });
+    }
+  );
 });
 
 app.post("/todo", (req, res) => {
+  console.log(req.body);
+  console.log(`INSERT INTO todos (todo) VALUES("${req.body.text}")`);
   pool.query(
-    "INSERT INTO todos (todo) VALUES(?)",
+    `INSERT INTO todos (todo) VALUES(?)`,
     req.body.text,
     (error, results, fields) => {
+      if (error) console.log(error);
+      console.log(results);
       res.send({ ok: true });
+    }
+  );
+});
+
+app.put("/todo/:id", (req, res) => {
+  pool.query(
+    "UPDATE todos SET todo=? WHERE id=?",
+    [req.body.text, req.params.id],
+    (error, results, fields) => {
+      console.log(results);
+      console.log(fields);
+      if (error) {
+        res.status(500).send({ ok: false, error: error });
+      } else {
+        res./*status(200).*/ send({ ok: true });
+      }
+    }
+  );
+});
+
+app.delete("/todo/:id", (req, res) => {
+  pool.query(
+    "DELETE FROM todos WHERE id=?",
+    req.params.id,
+    (error, results, fields) => {
+      console.log(results);
+      console.log(fields);
+      if (error) {
+        res.status(500).send({ ok: false, error: error });
+      } else {
+        res./*status(200).*/ send({ ok: true });
+      }
     }
   );
 });
